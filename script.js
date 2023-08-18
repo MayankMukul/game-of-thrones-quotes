d1f6c17fd83322216eea3a3dbd3ead8f5da979c4
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-08-18 18:35:33 +0530

    bootstrap added

---
+++ script.js
@@ -0,0 +1,11 @@
+let qot = document.querySelector("#qot");
+
+let url = "https://api.gameofthronesquotes.xyz/v1/random";
+
+
+
+function getQuote() {
+     fetch(url);
+     let p = new Promise()
+    
+}
