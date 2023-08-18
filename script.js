d9ab95af0649e72652a326679529079359290e4b
MayankMukul <58073442+MayankMukul@users.noreply.github.com>
2023-08-19 00:39:32 +0530

    js promise

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let qot = document.querySelector("#qot");
+let name1 = document.querySelector(".name");
 
 let url = "https://api.gameofthronesquotes.xyz/v1/random";
 
 
 
 function getQuote() {
-     fetch(url);
-     let p = new Promise()
+    
+     let p = new Promise((response,reject)=>{
+          response(fetch(url));
+     })
+     p.then((value)=>{
+          return value.json();
+     }).then((value)=>{
+          qot.innerHTML = `"${value.sentence}"`;
+          name1.innerHTML = `${value.character.name} in <cite title="Source Title">Game of Thrones</cite>`;
+         
+     })
     
 }
+
+getQuote();
